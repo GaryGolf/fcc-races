c08c5172541f940d8e06530a516066f78e1ae6c8
GaryGolf <garygolf@mail.ru>
2018-05-27 19:26:12 +0300

    create Race form component

---
+++ src/components/new-race-form/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+interface Props {
+  defaultValue?: string
+  onSubmit:(city:string)=>void;
+}
+
+interface State {
+  city: string;
+}
+
+export default class RaceForm extends React.Component<Props, State> {
+  
+  constructor(props: Props) {
+    super(props);
+    const city = props.defaultValue || '';
+    this.state = { city };
+  }
+
+  private handleInputChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const city = event.currentTarget.value;
+    this.setState({ city });
+  }
+
+  private handleKeyDown = (event:React.KeyboardEvent<HTMLInputElement>) => {
+    switch(event.key) {
+      case 'Enter' :
+        break;
+      case 'Escape' :
+        this.setState({ city: '' })
+      default:
+        break
+    }
+  }
+
+  private handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(this.state.city);
+  }
+
+  render() {
+    const value = this.state.city
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name="city"
+          value={this.state.city}
+          placeholder="city name"
+          onChange={this.handleInputChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
