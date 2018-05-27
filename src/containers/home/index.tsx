74dd4d0197fb2fa3002036652c39a3b86a6e9599
GaryGolf <garygolf@mail.ru>
2018-05-27 11:46:51 +0300

    minor changes

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -7,7 +7,7 @@ export default class HomePage extends React.PureComponent<Props, null> {
   render() {
     return (
       <div className={css.container}>
-        <h2> Races </h2>
+        <h2> Races start</h2>
       </div>
     )
   }
