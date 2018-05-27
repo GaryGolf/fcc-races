b59ec1333d613209b07c83f53457ee0a603600e2
GaryGolf <garygolf@mail.ru>
2018-05-27 10:43:05 +0300

    add production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,63 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
+const mode = process.env.NODE_ENV;
+
+const devCilentConfig = {
+  mode: 'development',
   entry: './src/client.tsx',
   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-      extensions: ['.js', '.ts', '.tsx']
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
-
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
+    hot: true,
     port: 3000
   },
+
+  module: {
+    rules: [
+   
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { test: /\.css$/, use: [ { loader: 'style-loader' } ,
+        { loader: 'css-loader', options: {
+          module: true,
+          sourceMap: true,
+          localIdentName: '[local]__[hash:base64:5]'
+        } }
+      ]}
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+const prodCilentConfig = {
+  mode: 'production',
+  entry: './src/client.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  devtool: false,
 
   module: {
     rules: [
@@ -28,7 +68,6 @@ module.exports = {
           loader: 'css-loader',
           query: {
             modules: true,
-            sourceMap: false,
             importLoaders: 1,
             localIdentName: '[local]__[hash:base64:5]'
           }
@@ -38,7 +77,9 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin("styles.css"),
-    new HtmlWebpackPlugin({ template: './src/index.html' })
+    new UglifyJsPlugin(),
+    new ExtractTextPlugin("styles.css")
   ]
 };
+
+module.exports = mode == 'production' ? devCilentConfig : devCilentConfig;
