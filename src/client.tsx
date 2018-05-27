9653c6ce7007b546a7fd8ac625327bd8300cd907
GaryGolf <garygolf@mail.ru>
2018-05-27 15:18:02 +0300

    add redux to client app

--- src/client.tsx
+++ src/client.tsx
@@ -3,9 +3,15 @@ import * as ReactDOM from 'react-dom';
 import { Router } from './helpers/router';
 import { BrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
+import { configureStore } from './store';
+import { Provider } from 'react-redux';
+
+const store = configureStore(window.__INITIAL_STATE__);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Router routes={routes}/>
-  </BrowserRouter>
+  <Provider store={store} key="provider">
+    <BrowserRouter>
+      <Router routes={routes}/>
+    </BrowserRouter>
+  </Provider>
 , document.getElementById('app'));
