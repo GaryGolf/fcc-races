ca05b13ea48b8ec3802bea43cfd07f6741e26443
GaryGolf <garygolf@mail.ru>
2018-06-01 20:46:51 +0300

    add selector

--- src/store/races/selector.ts
+++ src/store/races/selector.ts
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export const initialState:iRace[] = [
   { 
     num: 1,
@@ -11,3 +13,9 @@ export const initialState:iRace[] = [
     ] 
   }
 ];
+
+const getRaces = state => state.races;
+
+export const getLastRaceNum = createSelector (
+  [getRaces], races => races.reduce((acc, race) => Math.max(acc, race.num), 0)
+);
