cf18624094745cddeb3ee9d2d2927fe22369283b
GaryGolf <garygolf@mail.ru>
2018-06-02 09:26:00 +0300

    create new results form component

---
+++ src/components/race-results-form.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+interface Props {
+  num: number;
+}
+
+interface State {
+  isRacerForm: boolean;
+}
+
+export default class RaceResultsForm extends React.PureComponent<Props, State> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRacerForm: false
+    }
+  }
+  private handleFormSubmit = (event:React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  }
+
+  private handleNewRacerClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    this.setState({ isRacerForm: true })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleNewRacerClick}>Add racer</button>
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </div>
+    )
+  }
+
+}
+
+
+const AddRacerForm:React.SFC<{racers:string[]}> = props => {
+  return (
+    <div>
+      Form;
+    </div>
+  )
+}
